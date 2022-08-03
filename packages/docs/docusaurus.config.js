2494eefcc223851bbb2eb2366e23355d0d2b970a
vitalics <vitalicset@yandex.ru>
2022-08-03 04:40:41 +0400

    update url in docusaurus

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Telegraph',
   tagline: 'Dinosaurs are cool',
-  url: 'https://telegraph.com',
+  url: 'https://vitalics/Telegraph',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
